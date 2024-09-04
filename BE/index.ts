bf6ef2cb509e437e5240e9f1779dcf9283c989de
Ilnung Terl anofp Drohelviaskhf <vnitd@gmail.com>
2024-09-05 02:30:02 +0700

    Final fix 3

--- BE/index.ts
+++ BE/index.ts
@@ -23,7 +23,7 @@ connect(process.env.MONGODB_URL as string)
 /**
  * Get port from .env and store in Express.
  */
-const port: any = normalizePort(process.env.PORT || "3000");
+const port: any = normalizePort(process.env.PORT || "8080");
 app.set("port", port);
 
 /**
