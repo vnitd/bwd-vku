d749cf8baea2ad54405323c7b070eb72bc6d6b05
Ilnung Terl anofp Drohelviaskhf <vnitd@gmail.com>
2024-09-05 01:48:29 +0700

    Fix be 2

--- BE/dist/routes/users.route.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { register } from "../controllers/users.controller.js";
-const userRoute = Router();
-userRoute.post("/register", register);
-export default userRoute;
