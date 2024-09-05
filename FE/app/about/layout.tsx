8394312f6149818bbc2011651e4f3a747a6d32e6
thinhnguyenvku <fuocthinh411@gmail.com>
2024-09-06 00:38:45 +0700

    FE - push j4f

--- FE/app/about/layout.tsx
+++ FE/app/about/layout.tsx
@@ -1,13 +1,14 @@
+import React from "react";
+import "../../public/style/about.css"; // Import CSS file specific for the About page
+
 export default function AboutLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        {children}
-      </div>
+    <section className="about-layout-container">
+      <div className="about-content">{children}</div>
     </section>
   );
 }
